6e7ed80e2b0e501f8b7d4ce480d5990528317353
rawatakhilesh <akhileshrawat0501@gmail.com>
2018-11-16 17:54:26 -0600

    Update hawkeye_repl.h

--- hawkeye_repl.h
+++ hawkeye_repl.h
@@ -1,23 +1,45 @@
 #ifndef HAWKEYE_REPL_H_
 #define HAWKEYE_REPL_H_
 
+#include <vector>
+
 #include "repl_policies.h"
+
 
 // Hawkeye by Jain et al. 2016
 class HawkeyeReplPolicy : public ReplPolicy {
-protected:
+	protected:
+		uint64_t* array;
+        uint32_t numLines;
 
-
-public:
-
-    ~HawkeyeReplPolicy() {
+	public:
+        explicit HawkeyeReplPolicy(uint32_t _numLines) :numLines(_numLines) {
+            array = gm_calloc<uint64_t>(numLines);
+        }
+		
+		~HawkeyeReplPolicy() {
             gm_free(array);
         }
 
+        void update(uint32_t id, const MemReq* req) {
+			/* called on cache hit and on cache miss after rank, replaced are called*/   
+        }
 
-private:
+        void replaced(uint32_t id) {
+			/* called when there is a miss*/
+            
+        }
+
+        template <typename C> inline uint32_t rank(const MemReq* req, C cands) {
+			
+        }    
+		
+		DECL_RANK_BINDINGS;
+	
+	private:
+		//template <typename C> uint64_t {}
         
-        DECL_RANK_BINDINGS;
+       
 };
 #endif // HAWKEYE_REPL_H_
 
